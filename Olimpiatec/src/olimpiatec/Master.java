d023f7f8e0ee1053ec5e93808a90c7e933179923
Miguel <Miguel@Miguel-PC.gateway.2wire.net>
2017-02-21 23:09:37 -0600

    Avance

--- Olimpiatec/src/olimpiatec/Master.java
+++ Olimpiatec/src/olimpiatec/Master.java
@@ -11,27 +11,33 @@ import java.util.*;
  */
 public class Master {
     private String clave;
-    private Alumno alumnos;
+    private String matricula;
+    private ArrayList<Alumno> alumnos;
     private ArrayList<Evento> events;
 
-    public Master(String clave, Alumno alumnos, ArrayList<Evento> events) {
+    public Master(String clave, ArrayList<Alumno> alumnos, ArrayList<Evento> events) {
         this.clave = clave;
-        this.alumnos = alumnos;
+        this.matricula= null;
+        this.alumnos = new ArrayList<>();
         this.events = new ArrayList<>();
     }
     
-    public Master(String nombre, String apellido, String matricula, String carrera) {
-        this.clave = "";
-        this.alumnos = new Alumno(nombre, apellido, matricula, carrera);
-        this.events = new ArrayList<>();
-    }
     
     public Master(){
-        this.clave= "";
-        this.alumnos= new Alumno();
+        this.clave= null;
+        this.matricula=null;
+        this.alumnos= new ArrayList<>();
         this.events = new ArrayList<>();
     }
 
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    
     public String getClave() {
         return clave;
     }
@@ -40,13 +46,65 @@ public class Master {
         this.clave = clave;
     }
 
-    public Alumno getAlumnos() {
+    public ArrayList<Alumno> getAlumnos() {
         return alumnos;
     }
 
-    public void setAlumnos(Alumno alumnos) {
+    public void setAlumnos(ArrayList <Alumno> alumnos) {
         this.alumnos = alumnos;
     }
     
+    public ArrayList<Evento> getEvents(){
+        return events;
+    }
+    public void setEvents(ArrayList<Evento> events){
+        this.events=events;
+    }
     
-}
+    
+        
+    public boolean addEvent (Evento events){
+        if(!findClave(events.getClave())){
+            this.events.add(events);
+            return true;
+        }else{return false;}
+    }
+    
+    public boolean addEvent(String nombreEvento, String clave, String responsable){
+        if (!findClave(clave)){
+            this.events.add(new Evento(nombreEvento, clave, responsable) );
+            return true;
+        }else{return false;
+    }}
+    
+    public boolean removeEvent(String clave){
+        if (findClave(clave)){
+            ListIterator it = this.events.listIterator();
+            while (it.hasNext()){
+                Evento temp = (Evento) it.next();
+                
+                if(temp.getClave().equals(clave)){
+                    it.remove();
+                    return true;
+                }
+            }return true;
+        }else{return false;}
+    }
+
+    public boolean removeEvent (Evento events){
+        return removeEvent (events.getClave());
+    }
+    public boolean findClave(String clave){
+        if(!this.events.isEmpty()){
+            for (Evento tempEvent : events){
+                if(tempEvent.getClave().equals(clave)){
+                    return true;
+                }
+            } 
+        }return false;
+    }
+    
+    
+    
+    }
+
